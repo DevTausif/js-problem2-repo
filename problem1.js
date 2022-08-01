92ea7452e6d82c50daaf2bab0fbb362c6f3ee1e5
DevTausif <kabirtausif610@gmail.com>
2022-08-02 01:37:04 +0600

    practice problemt commit

---
+++ problem1.js
@@ -0,0 +1,14 @@
+// Find index number of banana
+var fruits =["Apple", "Banana", "Orange"];
+console.log(fruits[1]);
+
+// replace banana with mango
+fruits[1] = "Mango";
+console.log(fruits);
+
+// remove orange and add watermaleon
+fruits.pop();
+console.log(fruits);
+
+fruits.push("Watermaleon");
+console.log(fruits);
---
+++ problem2.js
@@ -0,0 +1,98 @@
+var grade = 85;
+
+if(grade <= 100 && grade >= 80){
+    console.log("My Grade is A");
+}
+else if(grade < 80 && grade >=60){
+    console.log("My Grade is B");
+
+}
+else if(grade < 60 && grade >=40){
+    console.log("My Grade is C");
+}
+else if( garde < 40 && grade >= 0){
+    console.log("My grade is D")
+}
+else{
+    console.log("fail");
+}
+
+// tom' grade
+var grade = 66;
+
+if(grade <= 100 && grade >= 80){
+    console.log("Tom's Grade is A");
+}
+else if(grade < 80 && grade >=60){
+    console.log("Tom's Grade is B");
+
+}
+else if(grade < 60 && grade >=40){
+    console.log("Tom's Grade is C");
+}
+else if( garde < 40 && grade >= 0){
+    console.log("Tom's grade is D")
+}
+else{
+    console.log("fail");
+}
+
+// Jane's Grade
+var grade = 96;
+
+if(grade <= 100 && grade >= 80){
+    console.log("Jane's Grade is A");
+}
+else if(grade < 80 && grade >=60){
+    console.log("Jane's Grade is B");
+
+}
+else if(grade < 60 && grade >=40){
+    console.log("Jane's Grade is C");
+}
+else if( garde < 40 && grade >= 0){
+    console.log("Jane's grade is D")
+}
+else{
+    console.log("fail");
+}
+
+//Peter's Grade
+var grade = 56;
+
+if(grade <= 100 && grade >= 80){
+    console.log("Peter's Grade is A");
+}
+else if(grade < 80 && grade >=60){
+    console.log("Peter's Grade is B");
+
+}
+else if(grade < 60 && grade >=40){
+    console.log("Peter's Grade is C");
+}
+else if( garde < 40 && grade >= 0){
+    console.log("Peter's grade is D")
+}
+else{
+    console.log("fail");
+}
+
+// John's Grade
+var grade = 40;
+
+if(grade <= 100 && grade >= 80){
+    console.log("John's Grade is A");
+}
+else if(grade < 80 && grade >=60){
+    console.log("John's Grade is B");
+
+}
+else if(grade < 60 && grade >=50){
+    console.log("John's Grade is C");
+}
+else if( grade > 39 && grade >= 40){
+    console.log("John's Grade is D");
+}
+else{
+    console.log("fail");
+}
